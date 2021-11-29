bd6a00c4b6c3bda086784a05b1dc465d9defad7d
刘斌 <liub19@xiaopeng.com>
2021-11-29 10:00:53 +0800

    init

---
+++ wsClient/src/main/java/com/test/wsClient/WsClientApplication.java
@@ -0,0 +1,12 @@
+package com.test.wsClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsClientApplication.class, args);
+	}
+
+}
